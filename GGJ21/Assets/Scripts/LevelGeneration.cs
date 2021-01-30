a141ccf9c55d3872f144c5440a0a0577dcc1580a
Jake Meaker <JM219785@falmouth.ac.uk>
2021-01-30 00:14:28 +0000

    Getting Maze generation set up.
    Currently generates one room and doesn't stay within the currently set bounds.
    Will add this functionality tomorrow

---
+++ GGJ21/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startPositions;
+    public GameObject[] rooms;
+
+    private int direction;
+    private const float moveAmount = 10;
+
+    private float timeBetweenRoom;
+    private float startTimeBetweemRoom = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randStartPos = Random.Range(0, startPositions.Length);
+        transform.position = startPositions[randStartPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Update()
+    {
+        if (timeBetweenRoom <= 0)
+        {
+            Move();
+            timeBetweenRoom = startTimeBetweemRoom;
+        }
+        else
+        {
+            timeBetweenRoom -= Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        if (direction == 1 || direction == 2)
+        {
+            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 3 || direction == 4)
+        {
+            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+            transform.position = newPos;
+        }
+        else if (direction == 5)
+        {
+            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
+    }
+}
