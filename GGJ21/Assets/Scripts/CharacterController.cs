bade44fc0adae26243a16ec15001885f207ecd04
Jake Meaker <JM219785@falmouth.ac.uk>
2021-01-30 12:24:40 +0000

    Added character controller

--- GGJ21/Assets/Scripts/CharacterController.cs
+++ GGJ21/Assets/Scripts/CharacterController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public float runSpeed = 20f;
+
+    private float horizontal;
+    private float vertical;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
