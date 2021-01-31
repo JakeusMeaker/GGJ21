666b2817ff09bb9887d95d5dc21723518489bfcc
JEMMA GILBERT <jg218537@falmouth.ac.uk>
2021-01-31 16:56:14 +0000

    Added start to Scene Manager.
    Signed-off-by: JEMMA GILBERT <jg218537@falmouth.ac.uk>

---
+++ GGJ21/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void WinScene()
+    {
+        SceneManager.LoadScene("SucessScene");
+    }
+
+    public void FailScene()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+}
